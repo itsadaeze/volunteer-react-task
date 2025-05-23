9433b2b9ff63c3769b680a598a82f95be11cb72d
itsadaeze <adaezeugwu6@gmail.com>
2025-05-23 09:42:05 +0100

    adjusted styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import Home from "./pages/Home";
 
 function App() {
   return(
-   <div className="bg-gradient-to-r from-blue-800 via-indigo-800 to-purple-800 text-white p-6 md:h-screen"> <Home /></div>
+<div className="bg-gradient-to-r from-blue-800 via-indigo-800 to-purple-800 text-white p-6 min-h-screen">
+  <Home />
+</div>
+
   )
 }
 
